f832e1abb44dba9adc91437b3b7c65d91f61f171
sambilokonga <abebawbegna@gmail.com>
2023-05-09 03:32:44 -0700

    Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
+import re
 import requests
-import sys
-"""
-Module to interface with the reddit api
-"""
 
 
-def count_words(subreddit, word_list=[], after=None, all_results=[]):
+def count_words(subreddit, word_list):
     """
-    Uses the reddit api to get a count of search terms from subreddit hot posts
+    GET the word count for each word in word_list.
+    Print results in descending order by the count, not the title.
+    If no posts match or subreddit is invalid, print a newline.
+    If a word has no matches, skip and do not print it.
     """
-    param = {}
-    new_after = None
-    if after is not None:
-        param = {'after': after}
-    url = 'https://reddit.com/r/' + subreddit + '/hot/.json'
-    headers = {'User-Agent': "lala"}
-    try:
-        r = requests.get(url, headers=headers, params=param)
-        new_after = r.json()['data'].get('after')
-        for data in r.json()['data'].get('children'):
-            all_results.append(data['data'].get('title'))
-    except:
-        return(None)
-    if new_after is not None:
-        return(count_words(subreddit, word_list, new_after, all_results))
-    else:
-        word_dict = dict.fromkeys((word_list.lower()), 0)
-        line = ""
-        for item in all_results:
-            line = item.lower()
-            for word in word_list:
-                if word in line:
-                    word_dict[word] += 1
-        for key in word_dict:
-            print("{:s}: {:d}".format(key, word_dict[key]))
-        return(all_results)
+    storage = {word.lower(): 0 for word in word_list}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'philsrequest'}
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    r = requests.get(url, headers=headers)
+    if (r.status_code == 404 or 'data' not in r.json()):
+        return None
     else:
-        print(len(count_words(sys.argv[1], [x for x in sys.argv[2].split()])))
+        while (1):
+            r = r.json()
+            for post in r['data']['children']:
+                tmp = post['data']['title'].split()
+                for word in tmp:
+                    if word.lower() in storage.keys():
+                        storage[word.lower()] += 1
+
+            after = r['data']['after']
+            if (after is None):
+                break
+            r = requests.get("{}?after={}".format(url, after), headers=headers)
+
+    storage = [(k, storage[k]) for k in
+               sorted(storage, key=storage.get, reverse=True)]
+
+    if len(storage) == 0:
+        print("")
+    else:
+        for k, v in storage:
+            if (v > 0):
+                print("{}: {:d}".format(k, v))
